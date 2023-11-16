8d033f00cd1610ee6864a852bf88d4859221fe9b
Christoph <christoph_rohde@icloud.com>
2023-11-16 21:22:40 +0100

    Outsource redundant AddPathButtonView in own View

--- GitCockpit/Views/AddPathButtonView.swift
+++ GitCockpit/Views/AddPathButtonView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct AddPathButtonView: View {
+    private let dictionaryPicker = DirectoryPicker()
+    @Environment(\.modelContext)
+    private var modelContext
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Add Path", action: {
+            if let dirPath = dictionaryPicker.chosePath() {
+                let newSearchPathModel = SearchPathModel(path: dirPath)
+                modelContext.insert(newSearchPathModel)
+
+                let gitDirectories = GitRepoHandler.listGitDirectories(from: dirPath)
+                let gitRepos = GitRepoHandler.createRepositoryModels(FromDirectories: gitDirectories)
+
+                for repo in gitRepos {
+                    print("Test: \(repo.name)")
+                }
+                // TODO: add to Env list
+            }
+        })
+        .keyboardShortcut("n", modifiers: [.command, .shift])
     }
 }
 
