ab12458e42f689e03c537711ab9fd93e43c47483
Christoph <christoph_rohde@icloud.com>
2023-11-16 21:24:01 +0100

    Add Project datat

---
+++ GitCockpit/Views/Utility/DictionaryPicker.swift
@@ -0,0 +1,31 @@
+//
+//  DirectoryPicker.swift
+//  Git Cockpit
+//
+//  Created by Christoph Rohde on 16.11.23.
+//
+
+import SwiftUI
+
+struct DirectoryPicker {
+    func chosePath(withDialogTitle dialogTitle: String = "Choose a Directory") -> String? {
+        let dialog = NSOpenPanel()
+
+        dialog.title = dialogTitle
+        dialog.showsResizeIndicator = true
+        dialog.showsHiddenFiles = false
+        dialog.allowsMultipleSelection = false
+        dialog.canChooseDirectories = true
+        dialog.canChooseFiles = false
+
+        if dialog.runModal() == NSApplication.ModalResponse.OK {
+            if let result = dialog.url { // Pathname of the file
+                return result.path
+
+                // path contains the file path e.g
+                // /Users/ourcodeworld/Desktop/tiger.jpeg
+            }
+        }
+        return nil
+    }
+}
