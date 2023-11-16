6076440ebea2518ba37620455d07d74d48a7d363
Christoph <christoph_rohde@icloud.com>
2023-11-16 21:16:49 +0100

    Add SearchPathModel

--- GitCockpit/Models/SearchPathModel.swift
+++ GitCockpit/Models/SearchPathModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class SearchPathModel: Identifiable, Hashable {
+    let path: String
+
+    init(path: String) {
+        self.path = path
+    }
+
+    static func == (lhs: SearchPathModel, rhs: SearchPathModel) -> Bool {
+        return lhs.path == rhs.path
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(path)
+    }
+}
