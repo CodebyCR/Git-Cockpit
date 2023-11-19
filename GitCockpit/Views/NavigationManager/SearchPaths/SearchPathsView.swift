2c221587e0bd105a43da6eb7e3c4b23c11fd5fc0
Christoph <christoph_rohde@icloud.com>
2023-11-19 18:50:00 +0100

    Add SearchPath Adding option to specific Toolbar

---
+++ GitCockpit/Views/NavigationManager/SearchPaths/SearchPathsView.swift
@@ -0,0 +1,39 @@
+//
+//  SearchPathsView.swift
+//  Git Cockpit
+//
+//  Created by Christoph Rohde on 16.11.23.
+//
+
+import SwiftData
+import SwiftUI
+
+struct SearchPathsView: View {
+    @Query(sort: \SearchPathModel.path, animation: .easeIn)
+    var searchPaths: [SearchPathModel]
+
+    var body: some View {
+        ZStack {
+            if searchPaths.isEmpty {
+                EmptyListView(
+                    message: "No search paths chosen",
+                    command: "⇧ ⌘ N")
+            } else {
+                List(searchPaths) { searchPathModel in
+                    Text(searchPathModel.path)
+                }
+            }
+        }
+        .toolbar {
+            Spacer()
+            AddPathButtonView()
+        }
+//        .onDelete(
+//            perform: deleteItems(with: <#T##[ItemIdentifierType]#>)
+//        )
+    }
+}
+
+#Preview {
+    SearchPathsView()
+}
