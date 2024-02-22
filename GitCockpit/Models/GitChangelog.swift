006194b4aa1238faaeb73485ba0b85b01d9d155e
Christoph <christoph_rohde@icloud.com>
2024-02-22 22:40:32 +0100

    Add vscode to .gitignore

---
+++ GitCockpit/Models/GitChangelog.swift
@@ -0,0 +1,8 @@
+//
+//  GitChangelog.swift
+//  Git Cockpit
+//
+//  Created by Christoph Rohde on 15.02.24.
+//
+
+import Foundation
---
+++ GitCockpit/Views/RepoInspectorView.swift
@@ -0,0 +1,18 @@
+//
+//  RepoInspectorView.swift
+//  Git Cockpit
+//
+//  Created by Christoph Rohde on 10.02.24.
+//
+
+import SwiftUI
+
+struct RepoInspectorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RepoInspectorView()
+}
