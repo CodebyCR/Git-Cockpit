3e5ecd012df81e895049022656c6a41a20de9fcf
Christoph <christoph_rohde@icloud.com>
2024-03-20 22:38:35 +0100

    Add SelectTagPopupView to select and deselect Tags

---
+++ GitCockpit/Views/Tags/View/SelectTagPopupView.swift
@@ -0,0 +1,47 @@
+//
+//  SelectTagPopupView.swift
+//  Git Cockpit
+//
+//  Created by Christoph Rohde on 20.03.24.
+//
+
+import SwiftData
+import SwiftUI
+
+struct SelectTagPopupView: View {
+    @Query(sort: \TagModel.name, animation: .easeInOut)
+    private let allTags: [TagModel]
+
+    @State
+    var selectedTags: [TagModel]
+
+    var body: some View {
+        List(allTags) { tag in
+            HStack {
+                SingleTagView(tag: tag)
+                Spacer()
+                if selectedTags.contains(tag) {
+                    Image(systemName: "checkmark")
+                        .foregroundColor(.green)
+                } else {
+                    Image(systemName: "xmark")
+                        .foregroundColor(.red)
+                }
+            }
+            .onTapGesture {
+                withAnimation(.spring(duration: 0.4)) {
+                    if selectedTags.contains(tag) {
+                        selectedTags.removeAll(where: { $0 == tag })
+                    } else {
+                        selectedTags.append(tag)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SelectTagPopupView(selectedTags: [])
+        .modelContainer(for: TagModel.self, inMemory: true)
+}
